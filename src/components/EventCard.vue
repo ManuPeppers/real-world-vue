4a29da913427cb71e9a8db000ea2623b35d1cb9c
Manuel Grimaldi <manuel.grimaldi@xpeppers.com>
2020-11-12 10:29:15 +0100

    initial setup app

---
+++ src/components/EventCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>
+    <div>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  </h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: 'qualcosa che non so'
+    }
+  }
+}
+</script>
+
+<style></style>
