54b7e17741f881541d7e664caa78b5ce5becef12
Manuel Grimaldi <manuel.grimaldi@xpeppers.com>
2020-11-17 15:01:41 +0100

    introducing slot into a component

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -6,8 +6,11 @@
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
-      <BaseIcon name="users" />
-      <span> {{ event.attendees.length }}</span>
+      <BaseIcon name="users"
+        ><div slot="slot1">
+          {{ event.attendees.length }} attending
+        </div></BaseIcon
+      >
     </div>
   </router-link>
 </template>
